e0be8274db253e995325506ae2526982151aacbf
Yididya <yidisam18@gmail.com>
2022-05-28 22:32:53 +0300

    added assets

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Home from './pages';
-import About from './pages/about';
-import Blogs from './pages/blog';
+// import Overview from './pages/overview';
+import Predict from './pages/predict';
+import Overview from './pages/overview';
+import Contact from './pages/contact';
 
 class App extends Component {
   render() {
@@ -15,9 +17,9 @@ class App extends Component {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          {/* <Route path="/contact" element={<Contact />} /> */}
-          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/overview" element={<Overview />} />
+          <Route path="/predict" element={<Predict />} />
+          <Route path="/contact" element={<Contact />} />
           {/* <Route path="/sign-up" element={<SignUp />} /> */}
         </Routes>
       </Router>
